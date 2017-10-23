cf8b222b23661c20cf01d3c5d849895c103dbd91
nicholasmf <celldeusuicide@hotmail.com>
2017-10-22 22:48:38 -0200

    always false predictor

--- javascript/processors/dummy/branch-predictor.js
+++ javascript/processors/dummy/branch-predictor.js
@@ -2,10 +2,22 @@ function AlwaysTruePredictor() {
     this.predict = function(instruction) {
         return instruction.params.branchTo;
     }
-    this.update = function(address, target, taken) {
+    this.update = function(instruction, taken) {
 
     }
     this.render = function(container) {
 
+    }
+}
+
+function AlwaysFalsePredictor() {
+    this.predict = function(instruction) {
+        return undefined;
+    }
+    this.update = function(instruction, taken) {
+
+    }
+    this.render = function(container) {
+        
     }
 }
