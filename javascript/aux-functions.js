04552a3129e05727cbfe077ba4cedcfcc6630d32
nicholasmf <celldeusuicide@hotmail.com>
2017-10-25 21:50:05 -0200

    fix on aux functions

--- javascript/aux-functions.js
+++ javascript/aux-functions.js
@@ -6,7 +6,7 @@ function isObject(elem) {
 // Returns value of elem - if elem is Register, calls get method; if elem is number returns itself
 function getValue(elem) 
 {
-    if (elem !== null && elem !== undefined && typeof elem === 'object')
+    if (elem !== null && elem !== undefined && typeof elem === 'object' && elem.get)
     {
         return elem.get();
     }
@@ -25,10 +25,10 @@ function checkRaW(i1, i2) {
     let i2source0 = isObject(i2.params.source) ? i2.params.source : {};
     let i2source1 = isObject(i2.params.source1) ? i2.params.source1 : {};
     let i2source2 = isObject(i2.params.source2) ? i2.params.source2 : {};
-    let i2source = (i2source0 || i2source1);
+    let i2source = i2source0.get !== undefined ? i2source0 : i2source1;
     let i2source2Exist = getValue(i2.params.source2) !== undefined;
 
-    if ((i2.params.type === DATA_TYPES.ARITHMETIC && i2source2Exist) && i1dest === i2dest) {
+    if ((i2.params.type === DATA_TYPES.ARITHMETIC && !i2source2Exist) && i1dest === i2dest) {
         return true;
     }
     if (i1dest === i2source || i1dest === i2source2) {
@@ -46,7 +46,7 @@ function checkWaR(i1, i2) {
     let i1source0 = isObject(i1.params.source) ? i1.params.source : {};
     let i1source1 = isObject(i1.params.source1) ? i1.params.source1 : {};
     let i1source2 = isObject(i1.params.source2) ? i1.params.source2 : {};
-    let i1source = (i1source0 || i1source1);
+    let i1source = i1source0.get !== undefined ? i1source0 : i1source1;
     let i1source2Exist = getValue(i1.params.source2) !== undefined;
 
     if ((i1.params.type === DATA_TYPES.ARITHMETIC && i1source2Exist) && i1dest === i2dest) {
